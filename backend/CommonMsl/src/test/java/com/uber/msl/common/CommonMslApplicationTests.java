db7afbca69598fd14630c74fe35f5d1e32f256f8
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-06-11 23:53:21 +0530

    added constancts for uber project common msl layer

---
+++ backend/CommonMsl/src/test/java/com/uber/msl/common/CommonMslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.msl.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonMslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
