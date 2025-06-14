31470d17cd4869880d01829d3a00d5b968b3ebbd
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-06-14 15:40:02 +0530

    added location service config classes

---
+++ backend/locationservice/src/main/java/com/uber/geo/locationservice/configurations/RedisConfiguration.java
@@ -0,0 +1,43 @@
+package com.uber.geo.locationservice.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+
+@EnableCaching
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration(){
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .enableTimeToIdle()
+                .entryTtl(Duration.ofMinutes(1))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
