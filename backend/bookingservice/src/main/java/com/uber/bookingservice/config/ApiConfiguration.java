4899e198845bef8c95c3302ebd3a993119a41b08
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-06-18 22:58:06 +0530

    added configuration for booking service

---
+++ backend/bookingservice/src/main/java/com/uber/bookingservice/config/ApiConfiguration.java
@@ -0,0 +1,8 @@
+package com.uber.bookingservice.config;
+
+import lombok.Generated;
+
+@Generated
+public interface ApiConfiguration {
+    public final String LOCATION_SERVICE_NAME="location";
+}
---
+++ backend/bookingservice/src/main/java/com/uber/bookingservice/config/RetrofitConfig.java
@@ -0,0 +1,42 @@
+package com.uber.bookingservice.config;
+
+import com.netflix.discovery.EurekaClient;
+import com.uber.bookingservice.common.AppConstants;
+import com.uber.bookingservice.ext.apis.LocationServiceApi;
+import jakarta.inject.Singleton;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Configuration
+public class RetrofitConfig {
+
+    private EurekaClient eurekaClient;
+
+    public RetrofitConfig(EurekaClient eurekaClient) {
+        this.eurekaClient = eurekaClient;
+    }
+
+    private String getServiceURL(String serviceName){
+        return eurekaClient.getNextServerFromEureka(serviceName,false).getHomePageUrl();
+    }
+
+    @Singleton
+    public Retrofit retrofit(){
+        return new Retrofit.Builder().baseUrl("").addConverterFactory(GsonConverterFactory.create()).build();
+    }
+
+    @Bean
+    public LocationServiceApi locationServiceApi(){
+        return new Retrofit.Builder()
+                .baseUrl(getServiceURL(AppConstants.LOCATION_SERVICE_NAME))
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient().newBuilder().build())
+                .build()
+                .create(LocationServiceApi.class);
+    }
+
+}
