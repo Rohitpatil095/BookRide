fbd927d00c34d2ed161cc3468e9dcf74d5e4435f
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-06-21 23:16:00 +0530

    added notification server for new ride book

---
+++ backend/NotificationServer/src/main/java/com/uber/notificationsocket/NotificationServerApplication.java
@@ -0,0 +1,13 @@
+package com.uber.notificationsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServerApplication.class, args);
+	}
+
+}
---
+++ backend/NotificationServer/src/test/java/com/uber/notificationsocket/NotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.notificationsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
