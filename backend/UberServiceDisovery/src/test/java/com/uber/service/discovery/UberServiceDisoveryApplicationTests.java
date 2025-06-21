89b48d8f53ba89f1ac5c480768bc1239fae2ce95
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-06-21 12:00:15 +0530

    added eureka discover serive for uber backend

---
+++ backend/UberServiceDisovery/src/test/java/com/uber/service/discovery/UberServiceDisoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.service.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberServiceDisoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
