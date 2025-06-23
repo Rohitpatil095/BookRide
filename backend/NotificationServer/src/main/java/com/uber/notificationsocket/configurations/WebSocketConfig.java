433c5025b3e5a4c1dad235ea5180e6310a589ff8
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-06-23 08:50:50 +0530

    added socket client js file for ride notification socket server

--- backend/NotificationServer/src/main/java/com/uber/notificationsocket/configurations/WebSocketConfig.java
+++ backend/NotificationServer/src/main/java/com/uber/notificationsocket/configurations/WebSocketConfig.java
@@ -4,18 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import java.util.List;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-         registry.addEndpoint("/ws").setAllowedOrigins("http://127.0.0.1:5500").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
