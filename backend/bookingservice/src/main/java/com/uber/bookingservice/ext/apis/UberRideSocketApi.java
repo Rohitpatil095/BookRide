0feaf09508fce879e5cca1d36724ca4c5c4bde13
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2025-06-30 22:02:59 +0530

    updated booking service for external ride socket api

--- backend/bookingservice/src/main/java/com/uber/bookingservice/ext/apis/UberRideSocketApi.java
+++ backend/bookingservice/src/main/java/com/uber/bookingservice/ext/apis/UberRideSocketApi.java
@@ -1,2 +1,12 @@
-package com.uber.bookingservice.ext.apis;public interface UberRideSocketApi {
+package com.uber.bookingservice.ext.apis;
+
+import com.uber.bookingservice.dto.RideRequestDto;
+import org.springframework.http.ResponseEntity;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface UberRideSocketApi {
+    @POST("/notify/newRide")
+    Call<Boolean> sendNewRideNotificationToNearByDrivers(@Body RideRequestDto rideRequestDto);
 }
